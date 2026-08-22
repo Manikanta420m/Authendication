56413c4fdb773003d4494ab4844dd6dea3df4551
Manikanta420m <manikanta420m@gmail.com>
2026-08-22 07:04:43 +0530

    "pagination"

--- controllers/image-controllers.js
+++ controllers/image-controllers.js
@@ -1,6 +1,7 @@
 const Image = require('../models/Image');
 const { uploadToCloudnary } = require('../helpers/cloudinary-helper');
 const fs=require('fs');
+const cloudinary = require('../config/cloudinary');
 
 const uploadImageController = async (req, res) => {
     try {
@@ -41,6 +42,9 @@ const uploadImageController = async (req, res) => {
 
 const fetchImagesController = async(req,res)=>{
   try{
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.page) || 5;
+    
      const images = await Image.find({});
      if(images){
       res.status(200).json({
@@ -61,7 +65,27 @@ const fetchImagesController = async(req,res)=>{
 
 const deleteImageController = async(req,res)=>{
     try{
-       
+        const getCurrentIdOfImage = req.params.id;
+        const userId = req.userInfo.userId;
+        const image = await Image.findById(getCurrentIdOfImage);
+        if(!image){
+            return res.status(404).json({
+               success : false,
+               message : 'Image not found'
+            });
+        }
+        if(image.uploadedBy.toString()!=userId){
+           return res.status(403).json({
+            success: false,
+            message: 'You are Not authorized to delete this Image!'
+          }); 
+        }
+      await cloudinary.uploader.destroy(image.publicId);
+      await Image.findByIdAndDelete(getCurrentIdOfImage);
+      res.status(200).json({
+        success : true,
+        message : 'Image deleted Successfully'
+      });
     }
     catch(e){
     console.log(e);
@@ -75,5 +99,6 @@ const deleteImageController = async(req,res)=>{
 
 module.exports = {
     uploadImageController,
-    fetchImagesController
+    fetchImagesController,
+    deleteImageController
 };
--- routes/image-routes.js
+++ routes/image-routes.js
@@ -3,13 +3,16 @@ const authMiddleware=require ('../middleware/auth-middleware');
 const adminMiddleware=require ('../middleware/admin-middleware');
 const uploadMiddleware = require('../middleware/upload-middleware');
 
-const {uploadImageController,fetchImagesController} = require('../controllers/image-controllers');
+const {uploadImageController,fetchImagesController,deleteImageController} = require('../controllers/image-controllers');
 
 const router = express.Router();
 
 
 router.post('/upload',authMiddleware,adminMiddleware,uploadMiddleware.single('image'),uploadImageController);
-router.get('/fetch',authMiddleware,adminMiddleware,uploadMiddleware.single('image'),fetchImagesController);
+router.get('/fetch',authMiddleware,fetchImagesController);
+router.delete('/:id',authMiddleware,adminMiddleware,deleteImageController);
 
 
 module.exports = router;
+
+//. 6a88b4054564df2fbae7d796
